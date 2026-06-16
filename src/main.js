09f69fa7d51b0aef00319b3d64f4b888987e3c09
Taras <157425421+OLadbe@users.noreply.github.com>
2026-06-16 16:00:09 +0200

    Disable Swiper functionality in main.js
    Comment out Swiper initialization and imports

--- src/main.js
+++ src/main.js
@@ -1,23 +1,23 @@
-import Swiper from 'swiper';
-import { Navigation, Pagination } from 'swiper/modules';
+// import Swiper from 'swiper';
+// import { Navigation, Pagination } from 'swiper/modules';
 
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
+// import 'swiper/css';
+// import 'swiper/css/navigation';
+// import 'swiper/css/pagination';
 
-const aboutSwiper = new Swiper('.about-swiper', {
-  modules: [Navigation, Pagination],
+// const aboutSwiper = new Swiper('.about-swiper', {
+//   modules: [Navigation, Pagination],
 
-  slidesPerView: 1,
-  spaceBetween: 24,
+//   slidesPerView: 1,
+//   spaceBetween: 24,
 
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
+//   pagination: {
+//     el: '.swiper-pagination',
+//     clickable: true,
+//   },
 
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-});
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
+// });
