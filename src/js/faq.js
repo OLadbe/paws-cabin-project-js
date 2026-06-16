e991f90a4fd8d4aac7c0fc139efdc1b71edb3848
innanaka <gordiienkoinna30@gmail.com>
2026-06-16 19:09:32 +0300

    added part of JS and fixed css

---
+++ src/js/faq.js
@@ -0,0 +1,15 @@
+import Accordion from 'accordion-js';
+
+import 'accordion-js/dist/accordion.min.css';
+
+new Accordion('.faq-list', {
+
+  duration: 300,
+
+  showMultiple: false,
+
+  openOnInit: [],
+
+  collapse: true,
+
+}); 
