16277b91aec39547f01475bfa1e7c8d64b237414
Taras <157425421+OLadbe@users.noreply.github.com>
2026-06-17 20:27:12 +0200

    Uncomment modal event listeners in navbar.js

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -1,13 +1,13 @@
-// const openModalBtn = document.querySelector('.burger-btn');
-// const closeModalBtn = document.querySelector('.backdrop-btn-close');
-// const backdrop = document.querySelector('.backdrop');
+const openModalBtn = document.querySelector('.burger-btn');
+const closeModalBtn = document.querySelector('.backdrop-btn-close');
+const backdrop = document.querySelector('.backdrop');
 
-// openModalBtn.addEventListener('click', () => {
-//   backdrop.classList.remove('is-hidden');
-//   document.body.style.overflow = 'hidden';
-// });
+openModalBtn.addEventListener('click', () => {
+  backdrop.classList.remove('is-hidden');
+  document.body.style.overflow = 'hidden';
+});
 
-// closeModalBtn.addEventListener('click', () => {
-//   backdrop.classList.add('is-hidden');
-//   document.body.style.overflow = '';
-// });
+closeModalBtn.addEventListener('click', () => {
+  backdrop.classList.add('is-hidden');
+  document.body.style.overflow = '';
+});
