316017e6bf0a2189d597710a2b499d7f27946e80
Taras <157425421+OLadbe@users.noreply.github.com>
2026-06-17 21:48:49 +0200

    Enhance modal behavior with link interactions
    Added functionality to close modal and scroll to target section when modal links are clicked.

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -1,6 +1,7 @@
 const openModalBtn = document.querySelector('.burger-btn');
 const closeModalBtn = document.querySelector('.backdrop-btn-close');
 const backdrop = document.querySelector('.backdrop');
+const modalLinks = document.querySelectorAll('.modal-link');
 
 openModalBtn.addEventListener('click', () => {
   backdrop.classList.remove('is-hidden');
@@ -10,4 +11,22 @@ openModalBtn.addEventListener('click', () => {
 closeModalBtn.addEventListener('click', () => {
   backdrop.classList.add('is-hidden');
   document.body.style.overflow = '';
+});
+
+modalLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    backdrop.classList.add('is-hidden');
+      document.body.style.overflow = '';
+    const targetId = link.getAttribute('href'); 
+    const targetSection = document.querySelector(targetId);
+    
+    if (targetSection) {
+      setTimeout(() => {
+        targetSection.scrollIntoView({
+          behavior: 'smooth', 
+          block: 'start'
+        });
+      }, 50);
+    }
+  });
 });
